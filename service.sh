63dfdd5f30b3e9b9cf962c249cd377a83a164aae
Pycm <6963578+PycmShoma@users.noreply.github.com>
2022-05-16 23:51:07 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,5 +1,14 @@
 #!/system/bin/sh
 #
-chmod 644 /sys/class/power_supply/bms/charge_full_design
-echo "40000000" > /sys/class/power_supply/bms/charge_full_design
-chmod 444 /sys/class/power_supply/bms/charge_full_design
+target=$(readlink -f /sys/class/power_supply/bms/charge_full_design)
+custom=/dev/.charge_full_design
+echo 4000000 > $custom
+/system/bin/mount -o bind $custom $target
+cat $target
+
+target=$(readlink -f /sys/class/power_supply/bms/uevent)
+custom=/dev/.uevent
+cat $target > $custom
+sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=4000000/' $custom
+/system/bin/mount -o bind $custom $target
+cat $target
